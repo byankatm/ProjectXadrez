0c2f71a04f9aac4b432d9277d144c5f577f8228f
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-15 16:58:15 -0300

    Update Main.java

--- Xadrez/Main.java
+++ Xadrez/Main.java
@@ -6,9 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+        while (true){
+            try{
+                UI.clearScreen();
+
+                System.out.println();
+
+                UI.printBoard(chessMatch.getPieces());
+
+                System.out.print("\nSource: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+                System.out.print("\nTarget: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.perfomeChessMove(source, target);
+
+            }
+            catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
     }
 
 }
