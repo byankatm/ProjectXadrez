ef8d27fb02b1b93299f68ae8a492156f89d5864c
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-16 08:39:31 -0300

    Update Main.java

--- Xadrez/Main.java
+++ Xadrez/Main.java
@@ -3,7 +3,9 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +15,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (true){
             try{
@@ -20,7 +23,7 @@ public class Main {
 
                 System.out.println();
 
-                UI.printMatch(chessMatch);
+                UI.printMatch(chessMatch, captured);
 
                 System.out.print("\nSource: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -33,7 +36,9 @@ public class Main {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = chessMatch.perfomeChessMove(source, target);
-
+                if (capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
             }
             catch (ChessException e){
                 System.out.println(e.getMessage());
