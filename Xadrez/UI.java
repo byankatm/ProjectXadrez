438d153d15f2e209937d733186a30a2525dca57f
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-16 08:39:15 -0300

    Update UI.java

--- Xadrez/UI.java
+++ Xadrez/UI.java
@@ -3,8 +3,11 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
 
@@ -44,8 +47,10 @@ public class UI {
         }
     }
 
-    public static void printMatch(ChessMatch chessMatch){
-          printBoard(chessMatch.getPieces());
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured){
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        printCapturedPieces(captured);
         System.out.println("\nTurn : "  + chessMatch.getTurn());
         System.out.println("Waiting player: " +  chessMatch.getCurrentPlayer());
     }
@@ -90,6 +95,23 @@ public class UI {
             }
         }
         System.out.print(' ');
+    }
+
+    private static void printCapturedPieces(List<ChessPiece> captured){
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).toList();
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).toList();
+
+        System.out.println("Captured pieces: ");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
+
     }
 
 }
