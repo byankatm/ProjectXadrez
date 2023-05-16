f4651ec87aad760be774a872ab9672934ed2e053
Byanka Tomaz <125160119+byankatm@users.noreply.github.com>
2023-05-16 08:22:31 -0300

    Update Main.java

--- Xadrez/Main.java
+++ Xadrez/Main.java
@@ -1,6 +1,10 @@
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -16,7 +20,7 @@ public class Main {
 
                 System.out.println();
 
-                UI.printBoard(chessMatch.getPieces());
+                UI.printMatch(chessMatch);
 
                 System.out.print("\nSource: ");
                 ChessPosition source = UI.readChessPosition(sc);
